0767effae828638b89b754e8d09f899d4668484d
cesar castro <you@example.com>
2022-04-26 02:30:15 -0300

    Buscar Persona

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Profile from './components/profile/profile.js';
+import React from 'react';
 
 function App() {
+  const [userId, setUser] = React.useState((Math.floor(Math.random() * (10 - 1 + 1) + 1)));
+  const setID = () => {
+    console.log(userId);
+    setUser((Math.floor(Math.random() * (10 - 1 + 1) + 1)));
+  }
   return (
-    
     <div className="App">
-      <Profile user={
-          {
-            name: 'Juan',
-            username: 'juanito',
-            email: 'adad@gmail.com',
-            photo: 'https://picsum.photos/200/300',
-        }}/>
+      <Profile userId = {userId}/>
+      <div className="button">
+        <button onClick={setID}>Cambiar Persona</button>
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -3,7 +3,7 @@
 
 import React from 'react';
 import './profile.css';
-
+const preID =null;
 export default class Profile extends React.Component {
     constructor(props) {
         super(props);
@@ -13,16 +13,45 @@ export default class Profile extends React.Component {
                     username: '',
                     email: '',
                     photo: '',
+                    userId: ''
                 },
         };
     }
     
     componentDidMount() {
-        this.setState({
-        user: this.props.user,
-        });
+        fetch('https://jsonplaceholder.typicode.com/users/' + this.props.userId)
+        .then(response => response.json())
+        .then(json => {
+            this.setState({
+                user: {
+                    name: json.name,
+                    username: json.username,
+                    email: json.email,
+                    photo: 'https://loremflickr.com/200/300/people?random=1',
+                    preID: this.props.userId
+                }
+            })
+        })
     }
-    
+
+    componentDidUpdate(){
+        if (this.props.userId !== preID) {
+            fetch('https://jsonplaceholder.typicode.com/users/' + this.props.userId)
+            .then(response => response.json())
+            .then(json => {
+                this.setState({
+                    user: {
+                        name: json.name,
+                        username: json.username,
+                        email: json.email,
+                        photo: "https://picsum.photos/200/300",
+                        preID : this.props.userId
+                    }
+                })
+            })
+        }
+    }
+
     render() {
         return (
             <div className="profile">
